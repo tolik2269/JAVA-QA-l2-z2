8dcb30c790f15f7c6f98a368598c131763a05afb
Anatoly Balbutsky <bal.anatolii1985@mail.ru>
2023-04-07 19:33:49 +0300

    l2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int firstAccount=100;
-        int streamMani=3000;
-        if (streamMani>1000){
-            System.out.println( streamMani/100+streamMani+firstAccount);
-    }else {
-            System.out.println(streamMani+firstAccount);
+        int firstAccount = 100;
+        int streamMani = 3000;
+        if (streamMani > 1000) {
+            System.out.println(streamMani / 100 + streamMani + firstAccount);
+        } else {
+            System.out.println(streamMani + firstAccount);
         }
     }
 }
